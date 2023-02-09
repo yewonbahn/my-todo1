493f9223d13fd3195de469f4a39dd9ec64269476
yewonbahn <banyaewon98@gmail.com>
2023-02-09 14:50:16 +0900

    feat: 4. 삭제 기능

--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -3,18 +3,25 @@
     <header><h1>Yewon todo1</h1></header>
     <main>
       <div class="todos">
-        <div class="write">
+        <div class="write" v-if="writeState=='add'">
           <input ref="writeArea" v-model="addItemText" type="text" @keyup.enter="addItem"/>
           <button class="btn add" @click="addItem">Add</button>
         </div>
+
+        <div class="write" v-else>
+          <input ref="writeArea" v-model="editItemText" type="text" @keyup.enter="addItem"/>
+          <button class="btn add" @click="editSave">Save</button>
+        </div>
+
+
         <ul  v-for="(item,i) in todos" class="list" :key="item.text">
           <li>
             <i  @click="checkItem(i)" :class="[item.state==='yet'? 'far':'fas','fa-check-square' ]"></i>
             <span>
               {{item.text}}
               <b>
-                <a href="">Edit</a>
-                <a href="">Del</a>
+                <a href="" @click.prevent="editShow(i)">Edit</a>
+                <a href="" @click.prevent="deleteItem(i)">Del</a>
               </b>
             </span>
           </li>
@@ -28,7 +35,10 @@
 export default {
     data(){
         return{
+          writeState:'add',
+          crrEditItem:'',
           addItemText:'',
+          editItemText:"",
           todos:[
           {text: '공부하기', state: 'yet'},
           {text: '운동하기', state: 'done'},
@@ -55,6 +65,19 @@ export default {
         }
   
       },
+      editShow(i){
+        this.crrEditItem= i;
+        this.writeState='edit';
+        this.editItemText=this.todos[i].text
+      },
+      editSave(){
+        this.todos[this.crrEditItem].text=this.editItemText;
+        this.writeState='add';
+      },
+      deleteItem(i){
+        this.todos.splice(i,1)
+      }
+      
 
     },
     mounted(){
