d9a0df114a05a887ae795477f83118e6ac478509
yewonbahn <banyaewon98@gmail.com>
2023-02-09 13:21:28 +0900

    feat: 2.할일 등록 기능

--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -4,12 +4,12 @@
     <main>
       <div class="todos">
         <div class="write">
-          <input  v-model="content" type="text"/>
-          <button class="btn add" @click="addClick">Add</button>
+          <input  v-model="addItemText" type="text"/>
+          <button class="btn add" @click="addItem">Add</button>
         </div>
         <ul v-for="item in todos" class="list" :key="item.text">
           <li>
-            <i  :class="[checkSquare[item.state],'fa-check-square' ]"></i>
+            <i  :class="[item.state==='yet'? 'far':'fas','fa-check-square' ]"></i>
             <span>
               {{item.text}}
               <b>
@@ -28,7 +28,7 @@
 export default {
     data(){
         return{
-          content:"",
+          addItemText:"",
           todos:[
           {text: '공부하기', state: 'yet'},
           {text: '운동하기', state: 'done'},
@@ -42,16 +42,9 @@ export default {
         }
     },
     methods:{
-      addClick(){
-        console.log(this.content)
-        this.todos.push({text:this.content,state:'yet'})
+      addItem(){
+        this.todos.push({text:this.addItemText,state:'yet'})
       },
-      select(ev){
-        console.log(ev.target)
-        ev.target.classList.remove('far fa-check-square');
-        console.log(ev.target)
-        ev.target.classList.add('fas fa-check-square');
-      }
     }
 
 }
